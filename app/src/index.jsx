396ccbbe8e11f6a33ea5a929cc3e695b05fb2d92
Justin Newberry <jnewberry0502@gmail.com>
2022-02-18 13:39:14 -0500

    add service worker

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -14,3 +14,13 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root')
 );
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+        .then(function (registration) {
+            console.log('Registration successful, scope is:', registration.scope);
+        })
+        .catch(function (error) {
+            console.log('Service worker registration failed, error:', error);
+        });
+}
