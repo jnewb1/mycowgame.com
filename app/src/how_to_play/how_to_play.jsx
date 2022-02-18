752ff12ddca9c39aacadc6259d33920365ca223d
Bennett Lawson <bclaw@umich.edu>
2022-02-18 18:50:56 +0000

    how to play v1

--- app/src/how_to_play/how_to_play.jsx
+++ app/src/how_to_play/how_to_play.jsx
@@ -18,31 +18,71 @@ const HowToPlay = function (props) {
       <div className="row margin-small">
         <h3>How To Play</h3>
       </div>
-      {/* <div className="row margin-large">
-        <p id="tutorial_intro_text">
-          Welcome to MyCows! Whether you're riding through the city or the
-          countryside, MyCows is the best way to pass the time with your
-          friends.
-        </p>
-      </div> */}
 
-      <div className="row margin-large tutorial-row">
+      <div className="row" style={{ textAlign: "center" }}>
+        <p>
+          First one to shout the correct phrase when one of the gamepieces is
+          spotted gets to perform an action
+        </p>
+      </div>
+
+      <div className="row tutorial-row">
         <div className="fa-icon tutorial-icon-container">
           <FontAwesomeIcon icon={faHorseHead} />
         </div>
 
         <div className="tutorial-description">
           <p>
-            <span className="bold">What: </span>Adds +1 cows to your total
+            <span className="bold">Phrase: </span>My Cow!
+          </p>
+          <p>
+            <span className="bold">What: </span>Add +1 to your score
           </p>
           <p>
             <span className="bold">When: </span>When you see a large animal
-            (think: larger than a German Sheopard)
           </p>
         </div>
       </div>
 
-      <div className="row margin-large">
+      <div className="row tutorial-row">
+        <div className="fa-icon tutorial-icon-container">
+          <FontAwesomeIcon icon={faChurch} />
+        </div>
+
+        <div className="tutorial-description">
+          <p>
+            <span className="bold">Phrase: </span>Marry My Cows!
+          </p>
+          <p>
+            <span className="bold">What: </span>Double your score
+          </p>
+          <p>
+            <span className="bold">When: </span>When you see a church
+          </p>
+        </div>
+      </div>
+
+      <div className="row tutorial-row">
+        <div className="fa-icon tutorial-icon-container">
+          <FontAwesomeIcon icon={faSkull} />
+        </div>
+
+        <div className="tutorial-description">
+          <p>
+            <span className="bold">Phrase: </span>Kill Your Cows!
+          </p>
+          <p>
+            <span className="bold">What: </span>Sets opponent's score to 0
+          </p>
+          <p>
+            <span className="bold">When: </span>When you see a graveyard
+          </p>
+        </div>
+      </div>
+
+      {/* Home Button */}
+
+      <div className="row margin-medium">
         <button className="main-menu-button" onClick={() => navigate("/")}>
           Return to Homepage
         </button>
