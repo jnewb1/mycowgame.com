a0db07a4308d6f5db86069360accf8b02895e6c8
SanchoPanza11 <93151004+bclawson3543@users.noreply.github.com>
2024-12-23 13:33:40 -0800

    Fix/qr background disable (#6)

--- src/modals/modals.jsx
+++ src/modals/modals.jsx
@@ -15,7 +15,7 @@ const QRModal = function (props) {
       </div>
 
       <div className="row margin-small">
-        <button className="error-button modal-button" onClick={confirmAction}>
+        <button className="button modal-button" onClick={confirmAction}>
           Confirm
         </button>
       </div>
@@ -24,10 +24,8 @@ const QRModal = function (props) {
   );
 };
 
-
 const ConfirmationModal = function (props) {
   const { prompt, cancelAction, confirmAction } = props;
-
   return (
     <div className="modal">
       <div className="row margin-small">
