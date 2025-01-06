31c5473917b6629f005dc064376f16678c910ef9
Justin Newberry <jnewberry0502@gmail.com>
2025-01-06 01:22:02 +0000

    fixes

--- src/modals/modals.jsx
+++ src/modals/modals.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { calculatePoints } from "../api"
 import distinctColors from "distinct-colors";
@@ -73,8 +73,8 @@ const AlertModal = function (props) {
 
 const ScoreGraphModal = function (props) {
     const { gameData, backAction } = props;
-    //aufmw 
-    let data = []
+
+    const [scoreGraph, setScoreGraph] = useState(null);
 
     const getGameDataSubset = (index) => {
         let subset = {};
@@ -83,38 +83,48 @@ const ScoreGraphModal = function (props) {
         return subset;
     }
 
-    gameData.actions.forEach((_, i) => {
-        let subsetGameData = getGameDataSubset(i);
-        calculatePoints(subsetGameData);
+    useEffect(() => {
+        let data = []
 
-        data.push({ "index": i });
-        subsetGameData.players.forEach((player) => {
-            data[i][player.name] = player.points
+        gameData.actions.forEach((_, i) => {
+            let subsetGameData = getGameDataSubset(i);
+            calculatePoints(subsetGameData);
+
+            data.push({ "index": i });
+            subsetGameData.players.forEach((player) => {
+                data[i][player.name] = player.points
+            })
         })
-    });
 
-    const palette = distinctColors({
-        count: gameData.players.length,
-        hueMin: 0,           // Minimum hue
-        hueMax: 360,         // Maximum hue
-        chromaMin: 50,       // Minimum chroma (vividness)
-        chromaMax: 80,       // Maximum chroma (vividness)
-        lightMin: 50,        // Minimum lightness
-        lightMax: 80,        // Maximum lightness
-    });
-    const playerColors = palette.map(color => color.hex());
+        const palette = distinctColors({
+            count: gameData.players.length,
+            hueMin: 0,           // Minimum hue
+            hueMax: 360,         // Maximum hue
+            chromaMin: 50,       // Minimum chroma (vividness)
+            chromaMax: 80,       // Maximum chroma (vividness)
+            lightMin: 50,        // Minimum lightness
+            lightMax: 80,        // Maximum lightness
+        });
 
-    return (
+        const scoreGraph = {
+            playerColors: palette.map(color => color.hex()),
+            data: data
+        }
+
+        setScoreGraph(scoreGraph);
+    }, [gameData])
+
+    return scoreGraph && (
         <div className="modal">
             <div className="row margin-small">
                 <div id="score_chart">
                     <ResponsiveContainer width="100%" height="100%">
-                        <LineChart data={data} margin={{ left: 10, right: 10, top: 10, bottom: 10 }}>
+                        <LineChart data={scoreGraph.data} margin={{ left: 10, right: 10, top: 10, bottom: 10 }}>
                             <CartesianGrid strokeDasharray="5 5" />
                             <XAxis dataKey="index" tick={{ fontSize: 40 }} />
                             <YAxis tick={{ fontSize: 40 }} />
                             {gameData.players.map((player, i) => (
-                                <Line type="monotone" key={player.name} dataKey={player.name} stroke={playerColors[i]} strokeWidth="8" />
+                                <Line type="monotone" key={player.name} dataKey={player.name} stroke={scoreGraph.playerColors[i]} strokeWidth="8" />
                             ))}
                             <Tooltip />
                             <Legend wrapperStyle={{ fontSize: 14 }} />
